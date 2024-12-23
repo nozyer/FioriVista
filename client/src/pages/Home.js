9058d1dbae64457720f301070add8e3e258125d5
nozyer <nozyer@sabanciuniv.edu>
2024-12-24 00:03:09 +0300

    feat:Home page updated with Navbar component and products added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,72 +1,39 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../contexts/AuthContext";
-import { checkIfUserAdmin } from "../services/api";
+import ProductCart from "../Components/ProductCart";
+import Loading from "./Loading";
+import { checkIfUserAdmin, getAllProducts } from "../services/api";
+import NavBar from "../Components/NavBar";
 
 function Home() {
-  const authContext = useContext(AuthContext);
-  const user = authContext.user;
-  const [role, setRole] = useState(null);
-  const navigate = useNavigate();
-
-  const checkUserRole = async () => {
-    if (user) {
-      const response = await checkIfUserAdmin(user.uid);
-      setRole(response.role);
+  const [products, setProducts] = useState();
+  const fetchProducts = async () => {
+    try {
+      const productList = await getAllProducts();
+      setProducts(productList);
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
     }
   };
-  useEffect(() => {
-    checkUserRole();
-  }, []);
 
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div>
-      <span>
-        {user ? user.email : <button>Please login or register</button>}
-      </span>
-      {role === "admin" ? (
-        <button onClick={() => navigate("/admindashboard")}>
-          Admin Dashboard
-        </button>
-      ) : (
-        <></>
-      )}
-      {!user ? (
-        <>
-          <button
-            className="w-full p-4 bg-red-500 text-white"
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            {" "}
-            Login
-          </button>
-          <button
-            className=" w-full p-4 bg-red-500 text-white"
-            onClick={() => {
-              navigate("/register");
-            }}
-          >
-            Register
-          </button>
-        </>
-      ) : (
-        <span></span>
-      )}
-      {user ? (
-        <button
-          className="w-full p-4 bg-red-500 text-white"
-          onClick={() => {
-            authContext?.logout();
-            navigate("/login");
-          }}
-        >
-          Logout
-        </button>
-      ) : (
-        <span></span>
-      )}
+    <div className="h-full mx-24 pb-10">
+      <NavBar />
+      <div>
+        {products ? (
+          <div className="grid grid-cols-2 gap-10 mt-8 lg:grid-cols-4">
+            {products.map((product, key) => (
+              <div key={key}>
+                <ProductCart props={product} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <Loading />
+        )}
+      </div>
     </div>
   );
 }
