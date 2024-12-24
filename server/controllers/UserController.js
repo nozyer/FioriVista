332765a6e80b345959d9bec67bcd34ebce1f0e3d
nozyer <nozyer@sabanciuniv.edu>
2024-12-24 19:05:00 +0300

    feat:backend updated

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -27,6 +27,62 @@ const checkIfUserAdmin = async (req, res, next) => {
   }
 };
 
+const getAllUsers = async (req, res, next) => {
+  try {
+    const usersSnapshot = await db.get();
+    const users = [];
+    usersSnapshot.forEach((doc) => {
+      users.push({ id: doc.id, ...doc.data() });
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+};
+const changeUserRole = async (req, res, next) => {
+  const { userId } = req.params;
+  const { newRole } = req.body;
+
+  try {
+    const userDocRef = db.doc(userId.trim());
+    await userDocRef.update({
+      userRole: newRole,
+    });
+    res.status(200).json(`User Role successfully changed to ${newRole}`);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to change userRole" });
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  const { userUid } = req.params;
+
+  try {
+    const userDocRef = db.doc(userUid.trim());
+    const userDoc = await userDocRef.get();
+
+    if (!userDoc.exists) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const userData = userDoc.data();
+    if (userData.userRole === "admin") {
+      return res.status(400).json({ message: "Admin users cannot be deleted." });
+    }
+
+    await userDocRef.delete();
+    await auth.deleteUser(userUid);
+
+    return res.status(200).json({ message: "User successfully deleted." });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error.", error });
+  }
+};
+
+
 module.exports = {
   checkIfUserAdmin,
+  getAllUsers,
+  changeUserRole,
+  deleteUser
 };
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
 const { checkIfUserAdmin } = require("../controllers/UserController");
+const { getAllUsers } = require("../controllers/UserController");
+const { changeUserRole } = require("../controllers/UserController");
+const { deleteUser } = require("../controllers/UserController");
 
 const router = express.Router();
 
 router.get("/checkIfUserAdmin/:userId", checkIfUserAdmin);
+router.get("/getAllUsers", getAllUsers);
+router.post("/changeUserRole/:userId", changeUserRole);
+router.delete("/deleteUser/:userUid", deleteUser);
+
 module.exports = router;
